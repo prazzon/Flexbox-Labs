81b9ee41e63e8a93c04ac37d8075f0c9cfb6d567
Prazzon <prazzon2@gmail.com>
2024-05-14 01:32:16 +0100

    Add initial loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Editor from "./components/Editor/Editor";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -5,6 +6,10 @@ import Playground from "./components/Playground/Playground";
 import Toolbar from "./components/Toolbar/Toolbar";
 
 function App() {
+   useEffect(() => {
+      document.body.classList.add("show");
+   })
+   
    return (
       <>
          <Header />
