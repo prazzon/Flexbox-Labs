a27727dcafd3af4d004fe4ae2db0f04dd5c88f9e
Prazzon <prazzon2@gmail.com>
2024-10-24 11:37:40 +0100

    style(modal): change modal initial and exit animations

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -16,9 +16,9 @@ const container = {
 };
 
 const modal = {
-   hidden: { opacity: 0, scale: 0 },
-   visible: { opacity: 1, scale: 1 },
-   exit: { opacity: 0, scale: 0 },
+   hidden: { opacity: 0, y: 30 },
+   visible: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+   exit: { opacity: 0, y: 30 },
 };
 
 interface Context {
