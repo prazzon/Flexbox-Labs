ebe1df9904f623c9c359886a7561fcef10130335
Prazzon <prazzon2@gmail.com>
2024-11-17 21:06:07 +0100

    refactor(toaster): extracted toaster logic into a custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { LayoutGroup } from "framer-motion";
 import { useEffect } from "react";
-import { Toaster } from "react-hot-toast";
+import CustomToaster from "./components/CustomToaster";
 import Header from "./components/Header/Header";
 import Playground from "./components/Playground/Playground";
 import SideBar from "./components/SideBar/SideBar";
-import { LayoutGroup } from "framer-motion";
 
 function App() {
    useEffect(() => {
@@ -17,24 +17,7 @@ function App() {
             <SideBar />
             <Playground />
          </LayoutGroup>
-         <Toaster
-            position="bottom-center"
-            reverseOrder={true}
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-               success: { duration: 3000 },
-               error: { duration: 2000 },
-               style: {
-                  maxWidth: "100%",
-                  borderRadius: "15px",
-                  padding: "12px 18px",
-                  backgroundColor: "#101114",
-                  color: "#fff",
-                  border: "1px solid rgba(0, 0, 0, 0.2)",
-               },
-            }}
-         />
+         <CustomToaster />
       </>
    );
 }
