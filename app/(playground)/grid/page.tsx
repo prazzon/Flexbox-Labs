8760e552ef4163aead54d26be0434030467d98b1
Prazzon <prazzon2@gmail.com>
2025-02-08 23:37:14 +0100

    feat(GridPage): add metadata for title and description

--- app/(playground)/grid/page.tsx
+++ app/(playground)/grid/page.tsx
@@ -1,4 +1,10 @@
 import GridPage from "@/app/_components/Pages/Grid/GridPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: "Flexbox labs | Grid",
+   description: "A visual tool for experimenting with grid layouts",
+};
 
 function Page() {
    return <GridPage />;
--- app/_components/Pages/Grid/GridPage.tsx
+++ app/_components/Pages/Grid/GridPage.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-/* eslint-disable react-refresh/only-export-components */
 import { useGrid } from "@/app/_hooks/useGrid";
-import { Metadata } from "next";
 import Playground from "../../Playground/Playground";
 import LayoutGroupWrapper from "../../UI/LayoutGroup";
 import MainContent from "../../UI/MainContent/MainContent";
 import GridArea from "./GridArea/GridArea";
 import GridSidebar from "./GridSidebar/GridSidebar";
 import GridToolbar from "./GridToolbar/GridToolbar";
-
-export const metadata: Metadata = {
-   title: "Flexbox labs | Grid",
-   description: "A visual tool for experimenting with grid layouts",
-};
 
 function GridPage() {
    const { container, clearSelected } = useGrid();
