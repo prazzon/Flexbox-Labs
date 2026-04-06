3fac3c60f29d869e02db1e4ffe5d2be6ddc239e6
Prazzon <prazzon2@gmail.com>
2026-04-06 16:07:34 +0100

    chore: update .nvmrc to specify Node.js version 22; adjust next.config.ts for SPA output and custom build directory

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,8 @@ const nextConfig: NextConfig = {
          },
       },
    },
+   output: "export", // Outputs a Single-Page Application (SPA).
+   distDir: "./dist", // Changes the build output directory to `./dist/`.
 };
 
 export default nextConfig;
